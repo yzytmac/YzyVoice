6f7c291cb26e7d3a323356ca739ba56a60b81eff
yzytmac <yzytmac@163.com>
2018-01-02 15:37:15 +0800

    添加mk注释

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,10 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
+#别人没有给我们源码，只给了so库，所以要预构建别人的so库，然后在构建自己的so库
+#使用预构建库fmod.so
 include $(CLEAR_VARS)
 LOCAL_MODULE := fmod-prebuilt
 LOCAL_SRC_FILES := libfmod.so
 include $(PREBUILT_SHARED_LIBRARY)
 
+#使用预构建库fmodL.so
 include $(CLEAR_VARS)
 LOCAL_MODULE := fmodL-prebuilt
 LOCAL_SRC_FILES := libfmodL.so
@@ -12,6 +15,7 @@ include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
+#构建自己的so库，并将预构建库构建的新库一并打包进来
 LOCAL_MODULE    := yzyvoice
 LOCAL_SRC_FILES := YzyVoice.cpp
 LOCAL_SHARED_LIBRARIES := fmod-prebuilt fmodL-prebuilt
